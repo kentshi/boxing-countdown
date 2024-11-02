5d021859f6593b062e9a338e6ed362553a829c6b
Kent <kenjishin@gmail.com>
2024-11-02 11:19:51 +1100

    first commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import './assets/index.css'
+import { createApp } from 'vue'
+import Boxing from './Boxing.vue'
+
+const boxing = createApp(Boxing);
+boxing.mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 5174,
+    strictPort: true,
+    hmr: {
+      port: 5174
+    }
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
